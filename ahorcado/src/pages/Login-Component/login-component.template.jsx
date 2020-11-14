7a9c7718443553b9d14b734886fe3ae88bccbcc9
Alberto <halbertoh3@gmail.com>
2020-11-14 12:18:35 +0100

    Bootstrap
    Probando bootstrap

--- ahorcado/src/pages/Login-Component/login-component.template.jsx
+++ ahorcado/src/pages/Login-Component/login-component.template.jsx
@@ -1,8 +1,21 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './login-component.css';
 
 export default () =>  {
     return (
-        <p>Login</p>
+        <body>
+            <Container fluid>
+                <Row>
+                    <Col md="12">
+                        <h1>AHORCADO</h1>
+                    </Col>
+                    <Col  md="12">
+                        Adfios
+                    </Col>
+                </Row>
+            </Container>
+        </body>
     );
 };
